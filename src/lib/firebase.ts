e580e453654256eeb0644a0244d1bf682f6c64fc
Hari Vinay <harivinay003@gmail.com>
2025-07-26 12:31:24 +0000

    no email still

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
@@ -12,12 +12,9 @@ const firebaseConfig = {
   "messagingSenderId": "473114562091"
 };
 
-let app;
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-}
-
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db };
+export { app, auth, db };
