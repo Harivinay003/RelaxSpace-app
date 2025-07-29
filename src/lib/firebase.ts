8c83686881785e569a77173912d8a6bfe6c4d8bd
Harivinay003 <136343330+Harivinay003@users.noreply.github.com>
2025-07-29 17:20:03 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   "projectId": "serenescape-1q8fb",
   "appId": "1:473114562091:web:5aea78f17005cdc4dea46e",
   "storageBucket": "serenescape-1q8fb.firebasestorage.app",
-  "apiKey": "AIzaSyD8HX9vM_DGZwdHzCU-ig9n_ZswbZqcAIs",
+   //insert your api key 
   "authDomain": "serenescape-1q8fb.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "473114562091"
